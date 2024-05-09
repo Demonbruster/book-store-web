f81e5d0e6531c358bf0f2edb74659473c56f83bc
suhail <demonbruster@gmail.com>
2024-05-09 23:39:36 +0530

    feat: 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+import { Box, Flex, Text } from "@mantine/core";
+
+export default function page() {
+  return <Flex h="100vh" w="100vw" justify="center" align="center">
+    <Text fz="xl" fw="bolder"> 404 </Text>
+  </Flex> 
+}

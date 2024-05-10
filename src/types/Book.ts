a6f1652d7be0bcd8f57d1b04a582f566970ee7a7
suhail <demonbruster@gmail.com>
2024-05-10 14:24:04 +0530

    type: book

--- src/types/Book.ts
+++ src/types/Book.ts
@@ -2,7 +2,7 @@ export interface IBook {
   id: number
   title: string
   author: string
-  publication_year: number
+  publication_year: string 
   genre: string[]
   description: string
   cover_image: string
